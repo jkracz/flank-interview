6716acff394c24bf0d7a98013eda1e20c2989deb
Joe Kracz <joseph.kracz@gmail.com>
2023-09-28 15:57:39 -0700

    removed unused state for now, while I get the site published

--- flank-interview-fe/src/App.js
+++ flank-interview-fe/src/App.js
@@ -1,41 +1,36 @@
-import { useState } from 'react';
-import './app.scss';
-import ThinTemplate from './templates/ThinTemplate/thin-template';
-import ReactMarkdown from 'react-markdown';
-import objective from './objective.js';
-import rehypeRaw from 'rehype-raw';
+import { useState } from "react";
+import "./app.scss";
+import ThinTemplate from "./templates/ThinTemplate/thin-template";
+import ReactMarkdown from "react-markdown";
+import objective from "./objective.js";
+import rehypeRaw from "rehype-raw";
 
 const options = [
-    { value: null, label: 'All' },
-    { value: 100, label: '$100' },
-    { value: 200, label: '$200' },
-    { value: 300, label: '$300' },
-    { value: 400, label: '$400' },
-    { value: 500, label: '$500' },
-]
+    { value: null, label: "All" },
+    { value: 100, label: "$100" },
+    { value: 200, label: "$200" },
+    { value: 300, label: "$300" },
+    { value: 400, label: "$400" },
+    { value: 500, label: "$500" },
+];
 
 function App() {
-    const [selected, setSelected] = useState([]);
+    // const [selected, setSelected] = useState([]);
 
     return (
         <ThinTemplate instructionTitle="Front-end Interview">
-            <div class='options'>
+            <div class="options">
                 {options.map((option) => (
-                    <button
-                        type='button'
-                        key={option.value}
-                    >
+                    <button type="button" key={option.value}>
                         {option.label}
                     </button>
                 ))}
             </div>
 
-            <ReactMarkdown rehypePlugins={[rehypeRaw]} className='markdown' children={objective} />
+            <ReactMarkdown rehypePlugins={[rehypeRaw]} className="markdown" children={objective} />
 
             <h2>Selected State</h2>
-            <pre>
-                {JSON.stringify(selected, null, 2)}
-            </pre>
+            <pre>{JSON.stringify(selected, null, 2)}</pre>
         </ThinTemplate>
     );
 }
